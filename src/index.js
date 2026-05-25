b939fd7e1e94e90cb1b64a690db00b28a0f3f603
Peter Jansne <peterjansen812@gmail.com>
2026-05-25 22:01:42 +0200

    attempt to deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Home from './pages/Home';
-import Layout from './Layout.js';
+import Layout from 'layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
