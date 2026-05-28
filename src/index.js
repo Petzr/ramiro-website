aff534a2408f19745e4e2d5464cbcce55a8278a9
Peter Jansne <peterjansen812@gmail.com>
2026-05-28 21:32:59 +0200

    Add react-router-dom with routes for / and /gallery

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.scss';
-import Home from './pages/Home';
 import Layout from './layout';
+import Home from './pages/Home';
+import Gallery from './pages/Gallery';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Layout>
-      <Home />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>
 );
